8ab13dba97a867964d14ab14deffcc938437835e
fathur0905 <93516834+fathur0905@users.noreply.github.com>
2021-11-02 16:06:41 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Run when the webpage is loaded
+window.addEventListener('load', () => {
+    Swal.fire({
+        title: 'Are you sure to play music in background?',
+        // text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector('.song').play();
+            resolveFetch().then(animationTimeline());
+        } else {
+            resolveFetch().then(animationTimeline());
+        }
+    })
+})
